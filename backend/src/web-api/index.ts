08d3ecbd2d606419cb29c2e901eb49af730c4e79
enqian <eqreguse@gmail.com>
2025-08-06 07:38:10 +0800

    Remove backend folder

--- backend/src/web-api/index.ts
+++
@@ -1,8 +0,0 @@
-export * as AdminBotWebApi from './admin/bots';
-export * as AdminOrderWebApi from './admin/orders';
-export * as AdminProductWebApi from './admin/products';
-
-export * as CustomerOrderWebApi from './customer/orders';
-export * as CustomerProductWebApi from './customer/products';
-
-export * as AuthApi from './auth';
